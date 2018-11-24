4067a4ac69c7ee4e2e562ee889051cab08694e13
sqsbfsi2018 <45263038+sqsbfsi2018@users.noreply.github.com>
2018-11-24 15:53:52 +0530

    script changes

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -1,18 +1,17 @@
 package TestRunner;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
 
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(plugin = {"html:target/cucumber-html-report", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"usage", "html:target/cucumber-html-report", "json:target/cucumber.json",
         "pretty:target/cucumber-pretty.txt",
         "junit:target/cucumber-results.xml"},
         features = "Feature",
-        glue = {"Stepdefinition"})
+        glue = {"Stepdefinition"},
+        monochrome = true)
 
 public class Runner {
 }
