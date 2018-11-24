e90f372857790fb8de725c78cdfcbe3b15eba7f0
sqsbfsi2018 <45263038+sqsbfsi2018@users.noreply.github.com>
2018-11-24 12:59:07 +0530

    Formating code

--- src/test/java/Stepdefinition/BuyerSteps.java
+++ src/test/java/Stepdefinition/BuyerSteps.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 
 public class BuyerSteps {
 
-	HomePage homePage = new HomePage();                                          
-    
-	 @Test//@Step                                                       
-	    public void opens_etsy_home_page() {
-	        homePage.open();
-	    }
-   
-  
+    HomePage homePage = new HomePage();
+
+    @Test//@Step
+    public void opens_etsy_home_page() {
+        homePage.open();
+    }
+
+
 }
